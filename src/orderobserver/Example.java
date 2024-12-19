f2b073b30f6d05199322e0067980ade02bd41847
desktop-c5hvom9\sbaker <sbaker@vermilionit.co.uk>
2024-12-19 21:04:50 +0000

    Additions and Changes

--- src/orderobserver/Example.java
+++ src/orderobserver/Example.java
@@ -7,15 +7,12 @@ public final class Example {
         KitchenPrinter kitchenPrinter = new KitchenPrinter();
         order.addObserver(kitchenPrinter);
 
-        Pizza pizza1 = new Pizza(new PizzaBase("Italian", 2.25d));
-        pizza1.addSauce("Tomato", 1.25d);
-        pizza1.addTopping("Pineapple", 2.30d);
-        pizza1.addCheese("Cheddar", 1.45d);
+        Pizza pizza1 = new Pizza("Italian", 2.25d);
+        pizza1.addSauce("Tomato", 1.25d).addTopping("Pineapple", 2.30d).addCheese("Cheddar", 1.45d);
         order.addPizza(pizza1);
 
-        Pizza pizza2 = new Pizza(new PizzaBase("Stuffed Crust", 4.70d));
-        pizza2.addSauce("Tomato", 1.25d);
-        pizza2.addTopping("Mushroom", 2.30d);
+        Pizza pizza2 = new Pizza("Stuffed Crust", 4.70d);
+        pizza2.addSauce("Tomato", 1.25d).addTopping("Mushroom", 2.30d);
         order.addPizza(pizza2);
 
         order.save();
--- src/orderobserver/Pizza.java
+++ src/orderobserver/Pizza.java
@@ -5,20 +5,28 @@ class Pizza {
 
     PizzaComponent components;
 
-    public Pizza(PizzaBase base) {
+    public Pizza(String baseName, double price) {
+
+        this(new PizzaBase(baseName, price));
+    }
+
+    private Pizza(PizzaBase base) {
         components = base;
     }
 
-    public void addSauce(String name, double price) {
+    public Pizza addSauce(String name, double price) {
         components = new Sauce(components, name, price);
+        return this;
     }
 
-    public void addTopping(String name, double price) {
+    public Pizza addTopping(String name, double price) {
         components = new Topping(components, name, price);
+        return this;
     }
 
-    public void addCheese(String name, double price) {
+    public Pizza addCheese(String name, double price) {
         components = new Cheese(components, name, price);
+        return this;
     }
 
     public String getDescription() {
